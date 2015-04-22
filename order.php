76f3b5905689b6683c4e979a0ea4266c45e4f2f4
Steve Geluso <stevegeluso@gmail.com>
2015-04-22 01:03:24 -0700

    adding secret key

--- order.php
+++ order.php
@@ -1,9 +1,10 @@
 <?php
+include('secret.php');
 require_once('stripe-php-2.1.2/init.php');
 
 // Set your secret key: remember to change this to your live secret key in production
 // See your keys here https://dashboard.stripe.com/account/apikeys
-\Stripe\Stripe::setApiKey("sk_test_rJQw13EMX93Xx4ONXU9M82WB");
+\Stripe\Stripe::setApiKey("$secret_key");
 
 // Get the credit card details submitted by the form
 $token = $_POST['stripeToken'];
